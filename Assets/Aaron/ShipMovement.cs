70cae95eec5299b5947fb6b5510e26f2ab6a6413
Aaron Zhang <aaronzhang@ChrisPersonalMac.local>
2023-09-16 13:42:20 -0400

    ship movement bounds and inflation logic

--- Assets/Aaron/ShipMovement.cs
+++ Assets/Aaron/ShipMovement.cs
@@ -10,11 +10,18 @@ public class ShipMovement : MonoBehaviour
 
     private Rigidbody _rigidbody;
     private Transform _transform;
+
+    private float _leftBoundary = -6;
+    private float _rightBoundary = 6;
+    private float _topBoundary = 18;
+    private float _bottomBoundary = 12;
+
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _transform = GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
@@ -26,7 +33,31 @@ public class ShipMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Debug.Log(_userHorizontalInput);
+        Debug.Log(_transform.position);
+        if (_transform.position.x <= _leftBoundary)
+        {
+            _rigidbody.velocity = new Vector3(0, _rigidbody.velocity.y, _rigidbody.velocity.z);
+            _userHorizontalInput = Mathf.Max(0, _userHorizontalInput);
+
+        }
+        if (_transform.position.x >= _rightBoundary)
+        {
+            _rigidbody.velocity = new Vector3(0, _rigidbody.velocity.y, _rigidbody.velocity.z);
+            _userHorizontalInput = Mathf.Min(0, _userHorizontalInput);
+
+        }
+        if (_transform.position.y >= _topBoundary)
+        {
+            _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0, _rigidbody.velocity.z);
+            _userHorizontalInput = Mathf.Max(0, _userVerticalInput);
+
+        }
+        if (_transform.position.y <= _bottomBoundary)
+        {
+            _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0, _rigidbody.velocity.z);
+            _userHorizontalInput = Mathf.Min(0, _userVerticalInput);
+        }
+
         _rigidbody.velocity += _transform.up * _userVerticalInput * _speedMultipler;
         _rigidbody.velocity += _transform.right * _userHorizontalInput * _speedMultipler;
     }
