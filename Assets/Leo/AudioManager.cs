7919b1965ad9e631335e8617e7e259a3c90f6a31
Leo Wang <leo.wang142001@gmail.com>
2023-09-16 13:58:24 -0400

    bgm

---
+++ Assets/Leo/AudioManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioClip mainMenuBGM;
+    [SerializeField] AudioClip inGameBGM;
+    [SerializeField] AudioClip gameOverBGM;
+
+    public bool toggle = true;
+
+    AudioSource source;
+
+    private void OnEnable()
+    {
+        SceneManager.activeSceneChanged += UpdateBGM;
+    }
+
+    public void UpdateBGM(Scene current, Scene next)
+    {
+        if (next.buildIndex == Constants.GameSceneIndex)
+        {
+            SetAndStartBGM(inGameBGM);
+        }
+        if (next.buildIndex == Constants.MainMenuSceneIndex)
+        {
+            SetAndStartBGM(mainMenuBGM);
+        }
+    }
+
+    void SetAndStartBGM(AudioClip clip)
+    {
+        source.Stop();
+        source.clip = clip;
+        source.Play();
+    }
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        SetAndStartBGM(mainMenuBGM);
+        DontDestroyOnLoad(this);
+    }
+
+    private void Update()
+    {
+        source.loop = toggle;
+    }
+}
